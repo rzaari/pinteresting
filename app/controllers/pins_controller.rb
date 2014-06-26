6ced1fa03b2f386e245ec679c3b8daff8753dfa0
Ryan Zaari <ryanzaari@gmail.com>
2014-06-26 01:25:59 -0700

    FINALE

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
 
 
   def index
-    @pins = Pin.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 6)
+    @pins = Pin.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
